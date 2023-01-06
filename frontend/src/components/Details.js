3d7b36a7a2c23a5d31a4d88063cfb0a8fbe52205
Naushad <17naushad2002@gmail.com>
2023-01-06 15:16:30 +0530

    few changes

--- frontend/src/components/Details.js
+++ frontend/src/components/Details.js
@@ -80,16 +80,15 @@ const Details = () => {
                     <div className="row">
                         <div className="left_view col-lg-6 col-md-6 col-12">
                             <img src="/profile.png" style={{ width: 50 }} alt="profile" />
-                            <h3 className="mt-3">Name: <span >{getuserdata.name}</span></h3>
-                            <h3 className="mt-3">Age: <span >{getuserdata.age}</span></h3>
-                            <p className="mt-3"><MailOutlineIcon />Email: <span>{getuserdata.email}</span></p>
-                            <p className="mt-3"><WorkIcon />Occuption: <span>{getuserdata.work}</span></p>
-                        </div>
-                        <div className="right_view  col-lg-6 col-md-6 col-12">
-
-                            <p className="mt-5"><PhoneAndroidIcon />mobile: <span>+91 {getuserdata.mobile}</span></p>
-                            <p className="mt-3"><LocationOnIcon />location: <span>{getuserdata.add}</span></p>
-                            <p className="mt-3">Description: <span>{getuserdata.desc}</span></p>
+                            <h3 className="mt-3">User: <span >{getuserdata.user}</span></h3>
+                            <h3 className="mt-3">DOB: <span >{getuserdata.DOB}</span></h3>
+                            <p className="mt-3"><MailOutlineIcon />Fullname: <span>{getuserdata.fullname}</span></p>
+                            <p className="mt-3"><WorkIcon />Mother Name: <span>{getuserdata.mother_name}</span></p>
+                            <p className="mt-3"><WorkIcon />products: <span>{getuserdata.products}</span></p>
+                            <p className="mt-3"><WorkIcon />hobbies: <span>{getuserdata.hobbies}</span></p>
+                            <p className="mt-3"><WorkIcon />state: <span>{getuserdata.state}</span></p>
+                            <p className="mt-3"><WorkIcon />city: <span>{getuserdata.city}</span></p>
+                            <p className="mt-3"><WorkIcon />Postal_code: <span>{getuserdata.Postal_code}</span></p>
                         </div>
                     </div>
 
--- frontend/src/components/Edit.js
+++ frontend/src/components/Edit.js
@@ -1,142 +1,240 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { NavLink, useParams, useNavigate } from 'react-router-dom'
-import { updatedata } from './context/ContextProvider'
-
+import React, { useContext, useEffect, useState } from "react";
+import { NavLink, useParams, useNavigate } from "react-router-dom";
+import { updatedata } from "./context/ContextProvider";
 
 const Edit = () => {
+  // const [getuserdata, setUserdata] = useState([]);
+  // console.log(getuserdata);
 
-    // const [getuserdata, setUserdata] = useState([]);
-    // console.log(getuserdata);
+  const { updata, setUPdata } = useContext(updatedata);
 
-   const {updata, setUPdata} = useContext(updatedata)
+  const naviagte = useNavigate("");
 
-    const naviagte = useNavigate("");
+  const [inpval, setINP] = useState({
+    user:"",
+    DOB:"", 
+    fullname:"" ,
+    mother_name:"",
+    products:"",
+    hobbies:"",
+    state:"",
+    city:"",
+    Postal_code:"",
+  });
 
-    const [inpval, setINP] = useState({
-        name: "",
-        email: "",
-        age: "",
-        mobile: "",
-        work: "",
-        add: "",
-        desc: ""
-    })
+  const setdata = (e) => {
+    console.log(e.target.value);
+    const { name, value } = e.target;
+    setINP((preval) => {
+      return {
+        ...preval,
+        [name]: value,
+      };
+    });
+  };
 
-    const setdata = (e) => {
-        console.log(e.target.value);
-        const { name, value } = e.target;
-        setINP((preval) => {
-            return {
-                ...preval,
-                [name]: value
-            }
-        })
+  const { id } = useParams("");
+  console.log(id);
+
+  const getdata = async () => {
+    const res = await fetch(`https://cloudwalker.onrender.com/getuser/${id}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const data = await res.json();
+    console.log(data);
+
+    if (res.status === 422 || !data) {
+      console.log("error ");
+    } else {
+      setINP(data);
+      console.log("get data");
     }
+  };
 
+  useEffect(() => {
+    getdata();
+  }, []);
 
-    const { id } = useParams("");
-    console.log(id);
+  const updateuser = async (e) => {
+    e.preventDefault();
 
+    const {
+      user,
+      DOB,
+      fullname,
+      mother_name,
+      products,
+      hobbies,
+      state,
+      city,
+      Postal_code,
+    } = inpval;
 
+    const res2 = await fetch(
+      `https://cloudwalker.onrender.com/updateuser/${id}`,
+      {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          user,
+          DOB,
+          fullname,
+          mother_name,
+          products,
+          hobbies,
+          state,
+          city,
+          Postal_code,
+        }),
+      }
+    );
 
-    const getdata = async () => {
+    const data2 = await res2.json();
+    console.log(data2);
 
-        const res = await fetch(`https://cloudwalker.onrender.com/getuser/${id}`, {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json"
-            }
-        });
-
-        const data = await res.json();
-        console.log(data);
-
-        if (res.status === 422 || !data) {
-            console.log("error ");
-
-        } else {
-            setINP(data)
-            console.log("get data");
-
-        }
+    if (res2.status === 422 || !data2) {
+      alert("fill the data");
+    } else {
+      naviagte("/");
+      setUPdata(data2);
     }
+  };
 
-    useEffect(() => {
-        getdata();
-    }, []);
+  return (
+    <div className="container">
+      <NavLink to="/">home2</NavLink>
+      <form className="mt-4">
+        <div className="row">
+        <div className="mb-3 col-lg-6 col-md-6 col-12">
+            <label htmlFor="exampleInputEmail1" className="form-label">
+              user
+            </label>
+            <input
+              type="text"
+              value={inpval.user}
+              onChange={setdata}
+              name="user"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+            />
+          </div>
+          <div className="mb-3 col-lg-6 col-md-6 col-12">
+            <label htmlFor="exampleInputPassword1" className="form-label">
+              DOB
+            </label>
+            <input
+              type="text"
+              value={inpval.DOB}
+              onChange={setdata}
+              name="DOB"
+              className="form-control"
+              id="exampleInputPassword1"
+            />
+          </div>
+          <div className="mb-3 col-lg-6 col-md-6 col-12">
+            <label htmlFor="exampleInputPassword1" className="form-label">
+              fullname
+            </label>
+            <input
+              type="text"
+              value={inpval.fullname}
+              onChange={setdata}
+              name="fullname"
+              className="form-control"
+              id="exampleInputPassword1"
+            />
+          </div>
+          <div className="mb-3 col-lg-6 col-md-6 col-12">
+            <label htmlFor="exampleInputPassword1" className="form-label">
+              mother_name
+            </label>
+            <input
+              type="text"
+              value={inpval.mother_name}
+              onChange={setdata}
+              name="mother_name"
+              className="form-control"
+              id="exampleInputPassword1"
+            />
+          </div>
+          <div className="mb-3 col-lg-6 col-md-6 col-12">
+            <label htmlFor="exampleInputPassword1" className="form-label">
+              products
+            </label>
+            <input
+              type="text"
+              value={inpval.products}
+              onChange={setdata}
+              name="products"
+              className="form-control"
+              id="exampleInputPassword1"
+            />
+          </div>
+          <div className="mb-3 col-lg-6 col-md-6 col-12">
+            <label htmlFor="exampleInputPassword1" className="form-label">
+              hobbies
+            </label>
+            <input
+              type="text"
+              value={inpval.hobbies}
+              onChange={setdata}
+              name="hobbies"
+              className="form-control"
+              id="exampleInputPassword1"
+            />
+          </div>
+          <div className="mb-3 col-lg-6 col-md-6 col-12">
+            <label htmlFor="exampleInputPassword1" className="form-label">
+              state
+            </label>
+            <input
+              type="text"
+              value={inpval.state}
+              onChange={setdata}
+              name="state"
+              className="form-control"
+              id="exampleInputPassword1"
+            ></input>
+          </div>
+          <div className="mb-3 col-lg-6 col-md-6 col-12">
+            <label htmlFor="exampleInputPassword1" className="form-label">
+              city
+            </label>
+            <input
+              name="city"
+              value={inpval.city}
+              onChange={setdata}
+              className="form-control"
+              id="exampleInputPassword1"
+            ></input>
+          </div>
+          <div className="mb-3 col-lg-6 col-md-6 col-12">
+            <label htmlFor="exampleInputPassword1" className="form-label">
+              Postal_code
+            </label>
+            <input
+              name="Postal_code"
+              value={inpval.Postal_code}
+              onChange={setdata}
+              className="form-control"
+              id="exampleInputPassword1"
+            ></input>
+          </div>
 
-
-    const updateuser = async(e)=>{
-        e.preventDefault();
-
-        const {name,email,work,add,mobile,desc,age} = inpval;
-
-        const res2 = await fetch(`https://cloudwalker.onrender.com/updateuser/${id}`,{
-            method: "PATCH",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body:JSON.stringify({
-                name,email,work,add,mobile,desc,age
-            })
-        });
-
-        const data2 = await res2.json();
-        console.log(data2);
-
-        if(res2.status === 422 || !data2){
-            alert("fill the data");
-        }else{
-            naviagte("/")
-            setUPdata(data2);
-        }
-
-    }
-
-    return (
-        <div className="container">
-            <NavLink to="/">home2</NavLink>
-            <form className="mt-4">
-                <div className="row">
-                    <div class="mb-3 col-lg-6 col-md-6 col-12">
-                        <label for="exampleInputEmail1" class="form-label">Name</label>
-                        <input type="text" value={inpval.name} onChange={setdata} name="name" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-                    </div>
-                    <div class="mb-3 col-lg-6 col-md-6 col-12">
-                        <label for="exampleInputPassword1" class="form-label">email</label>
-                        <input type="email" value={inpval.email} onChange={setdata} name="email" class="form-control" id="exampleInputPassword1" />
-                    </div>
-                    <div class="mb-3 col-lg-6 col-md-6 col-12">
-                        <label for="exampleInputPassword1" class="form-label">age</label>
-                        <input type="text" value={inpval.age} onChange={setdata} name="age" class="form-control" id="exampleInputPassword1" />
-                    </div>
-                    <div class="mb-3 col-lg-6 col-md-6 col-12">
-                        <label for="exampleInputPassword1" class="form-label">Mobile</label>
-                        <input type="number" value={inpval.mobile} onChange={setdata} name="mobile" class="form-control" id="exampleInputPassword1" />
-                    </div>
-                    <div class="mb-3 col-lg-6 col-md-6 col-12">
-                        <label for="exampleInputPassword1" class="form-label">Work</label>
-                        <input type="text" value={inpval.work} onChange={setdata} name="work" class="form-control" id="exampleInputPassword1" />
-                    </div>
-                    <div class="mb-3 col-lg-6 col-md-6 col-12">
-                        <label for="exampleInputPassword1" class="form-label">Address</label>
-                        <input type="text" value={inpval.add} onChange={setdata} name="add" class="form-control" id="exampleInputPassword1" />
-                    </div>
-                    <div class="mb-3 col-lg-12 col-md-12 col-12">
-                        <label for="exampleInputPassword1" class="form-label">Description</label>
-                        <textarea name="desc" value={inpval.desc} onChange={setdata} className="form-control" id="" cols="30" rows="5"></textarea>
-                    </div>
-
-                    <button type="submit" onClick={updateuser} class="btn btn-primary">Submit</button>
-                </div>
-            </form>
+          <button type="submit" onClick={updateuser} class="btn btn-primary">
+            Submit
+          </button>
         </div>
-    )
-}
+      </form>
+    </div>
+  );
+};
 
 export default Edit;
-
-
-
-
-
